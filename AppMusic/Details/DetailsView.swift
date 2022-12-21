12fab17eebc02982ef935f8d5093eed5e92c0928
Joao-Uchoas <jvuchoas1998@gmail.com>
2022-12-21 10:20:19 -0300

    updated files

---
+++ AppMusic/Details/DetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsView.swift
+//  AppMusic
+//
+//  Created by Uchoas Santos, Joao Vitor on 20/12/22.
+//
+
+import UIKit
+
+class DetailsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AppMusic/Details/DetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsViewController.swift
+//  AppMusic
+//
+//  Created by Uchoas Santos, Joao Vitor on 20/12/22.
+//
+
+import UIKit
+
+class DetailsViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
